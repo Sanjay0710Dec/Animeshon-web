35f815cd93cc225051d0e69f3e6c6f32bce605b9
Sanjay0710Dec <sanjaymike0714@gmail.com>
2024-12-03 13:44:01 +0530

    (fallbacks): removed text color conflict

--- src/components/fallbacks/FallBackSub.tsx
+++ src/components/fallbacks/FallBackSub.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FallBackSub: React.FC = () => {
   return (
-    <main className="main-container bg-black text-white flex items-center justify-center text-white text-2xl lg:text-3xl">
+    <main className="main-container bg-black  flex items-center justify-center text-white text-2xl lg:text-3xl">
       loading...
     </main>
   );
