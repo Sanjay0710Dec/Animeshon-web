9dfbc6e505cc558347b9ac136616ce28f36bb9d4
Sanjay0710Dec <sanjaymike0714@gmail.com>
2024-12-04 11:05:27 +0530

    (hooks): removed unwanted logs

--- src/hooks/useControlVideo.ts
+++ src/hooks/useControlVideo.ts
@@ -2,11 +2,11 @@ import Hls from "hls.js";
 import { VideoQuality } from "../types/animeResult/animeStream";
 import React, { useRef, useState, useEffect, useCallback } from "react";
 
-const leadingZeroFormatter = new Intl.NumberFormat(undefined,{
-  minimumIntegerDigits:2,
-})
+const leadingZeroFormatter = new Intl.NumberFormat(undefined, {
+  minimumIntegerDigits: 2,
+});
 const useControlVideo = (src: string) => {
-  const[videoLoading,setVideoLoading] = useState(false);
+  const [videoLoading, setVideoLoading] = useState(false);
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const videoContainerRef = useRef<HTMLSelectElement | null>(null);
   const hlsRef = useRef<Hls | null>(null);
@@ -16,10 +16,10 @@ const useControlVideo = (src: string) => {
   const [videoPaused, setVideoPaused] = useState(true);
   const [currentVolume, setCurrentVolume] = useState(100);
   const [videoProgress, setVideoProgess] = useState(0);
-  const[fullScreen,setFullScreen] = useState(false);
-  const[showSetting,setShowSettings] = useState(false);
-  const[totalDuration,setTotalDuration] = useState("");
-  const[currentDuration,setCurrentDuration] = useState("");
+  const [fullScreen, setFullScreen] = useState(false);
+  const [showSetting, setShowSettings] = useState(false);
+  const [totalDuration, setTotalDuration] = useState("0:00");
+  const [currentDuration, setCurrentDuration] = useState("0:00");
 
   const handleVolumeChange = useCallback(function (
     e: React.ChangeEvent<HTMLInputElement>,
@@ -33,24 +33,17 @@ const useControlVideo = (src: string) => {
     }
   }, []);
 
-
-
-  const handleVolumeMute = useCallback(function(){
-  
-    if(videoRef.current){
-      
-      if(videoRef.current.muted){
-           videoRef.current.muted = !videoRef.current.muted;
-           setCurrentVolume(videoRef.current.volume * 100);
-      }
-      else{
+  const handleVolumeMute = useCallback(function () {
+    if (videoRef.current) {
+      if (videoRef.current.muted) {
+        videoRef.current.muted = !videoRef.current.muted;
+        setCurrentVolume(videoRef.current.volume * 100);
+      } else {
         videoRef.current.muted = !videoRef.current.muted;
         setCurrentVolume(0);
-        
-
       }
     }
-},[])
+  }, []);
 
   const skipVideo5seconds = useCallback(function (time: 5 | -5) {
     if (videoRef.current) {
@@ -59,62 +52,64 @@ const useControlVideo = (src: string) => {
   }, []);
 
   const changeQuality = useCallback(function (level: number) {
-      if(hlsRef.current){
-         hlsRef.current.currentLevel = level;
-      }
+    if (hlsRef.current) {
+      hlsRef.current.currentLevel = level;
+    }
   }, []);
 
   const togglePlayPause = useCallback(function () {
     if (videoRef.current) {
       if (videoRef.current.paused) {
-      
         videoRef.current.play();
         setVideoPaused(false);
       } else {
-       
         videoRef.current.pause();
         setVideoPaused(true);
       }
     }
   }, []);
 
-
   const handleVideoTimeUpdate = useCallback(function () {
     if (videoRef.current) {
-
       const currentProgress =
         (videoRef.current.currentTime / videoRef.current.duration) * 100;
       setVideoProgess(currentProgress);
-       formatDuration(videoRef.current.currentTime,true);
+      formatDuration(videoRef.current.currentTime, true);
     }
   }, []);
 
-  
-const formatDuration = useCallback(function(time:number,isCalledToUpdateCurrentDuration:boolean = false){
-       
-    if(videoRef.current){
-       const seconds = Math.floor(time % 60);
-       const minutes = Math.floor(time / 60) % 60;
-       const hours = Math.floor(time/ 3600);
-       
-       if(isCalledToUpdateCurrentDuration){
-        if( hours === 0){
-          setCurrentDuration(`${minutes}:${leadingZeroFormatter.format(seconds)}`);
+  const formatDuration = useCallback(function (
+    time: number,
+    isCalledToUpdateCurrentDuration: boolean = false,
+  ) {
+    if (videoRef.current) {
+      const seconds = Math.floor(time % 60);
+      const minutes = Math.floor(time / 60) % 60;
+      const hours = Math.floor(time / 3600);
+
+      if (isCalledToUpdateCurrentDuration) {
+        if (hours === 0) {
+          setCurrentDuration(
+            `${minutes}:${leadingZeroFormatter.format(seconds)}`,
+          );
+        } else {
+          setCurrentDuration(
+            `${hours}:${leadingZeroFormatter.format(minutes)}:${leadingZeroFormatter.format(seconds)}`,
+          );
         }
-        else{
-           setCurrentDuration(`${hours}:${leadingZeroFormatter.format(minutes)}:${leadingZeroFormatter.format(seconds)}`);
+      } else {
+        if (hours === 0) {
+          setTotalDuration(
+            `${minutes}:${leadingZeroFormatter.format(seconds)}`,
+          );
+        } else {
+          setTotalDuration(
+            `${hours}:${leadingZeroFormatter.format(minutes)}:${leadingZeroFormatter.format(seconds)}`,
+          );
         }
-       }
-       else{
-        if( hours === 0){
-          setTotalDuration(`${minutes}:${leadingZeroFormatter.format(seconds)}`);
-        }
-        else{
-           setTotalDuration(`${hours}:${leadingZeroFormatter.format(minutes)}:${leadingZeroFormatter.format(seconds)}`);
-        }
-       }
+      }
     }
-},[]);
+  }, []);
 
   const handleInputProgressBarChange = useCallback(function (
     e: React.ChangeEvent<HTMLInputElement>,
@@ -130,140 +125,125 @@ const formatDuration = useCallback(function(time:number,isCalledToUpdateCurrentD
     }
   }, []);
 
-  const toggleFullScreen = useCallback(function(){
-          
-     if(document.fullscreenElement == null){
-         
-        if(videoContainerRef.current){
-          videoContainerRef.current.requestFullscreen();
-        }
-     }
-     else{
+  const toggleFullScreen = useCallback(function () {
+    if (document.fullscreenElement == null) {
+      if (videoContainerRef.current) {
+        videoContainerRef.current.requestFullscreen();
+      }
+    } else {
       document.exitFullscreen();
-     }
-  },[])
+    }
+  }, []);
 
-  const handleKeyDown = useCallback(function(e: React.KeyboardEvent<HTMLSelectElement>){
-        
-         const tagName = document.activeElement?.tagName.toLowerCase();
-         if(tagName === "button"){
-          return;
-         }
-         
-        switch(e.key.toLowerCase()){
-            case " ":
+  const handleKeyDown = useCallback(function (
+    e: React.KeyboardEvent<HTMLSelectElement>,
+  ) {
+    const tagName = document.activeElement?.tagName.toLowerCase();
+    if (tagName === "button") {
+      return;
+    }
 
-              togglePlayPause();
-              break;
+    switch (e.key.toLowerCase()) {
+      case " ":
+        togglePlayPause();
+        break;
 
-            case "k":
-              togglePlayPause();
-              break;
-            case "l":
-              skipVideo5seconds(5);
-              break;
-            case "j":
-              skipVideo5seconds(-5);
-              break;
-            case "arrowleft":
-              skipVideo5seconds(-5);
-              break;
-            case "arrowright":
-               skipVideo5seconds(5);
-               break;
-            case "m":
-              handleVolumeMute();
-              break;
-            case "f":
-              toggleFullScreen();
-              break;    
-              
-        }
-  },[]);
+      case "k":
+        togglePlayPause();
+        break;
+      case "l":
+        skipVideo5seconds(5);
+        break;
+      case "j":
+        skipVideo5seconds(-5);
+        break;
+      case "arrowleft":
+        skipVideo5seconds(-5);
+        break;
+      case "arrowright":
+        skipVideo5seconds(5);
+        break;
+      case "m":
+        handleVolumeMute();
+        break;
+      case "f":
+        toggleFullScreen();
+        break;
+    }
+  }, []);
 
- 
+  useEffect(function () {
+    function handleFullScreenChange() {
+      setFullScreen(document.fullscreenElement !== null);
+    }
+    document.addEventListener("fullscreenchange", handleFullScreenChange);
 
-
-  useEffect(function(){
-
-       function handleFullScreenChange(){
-          setFullScreen(document.fullscreenElement !== null);
-       }
-       document.addEventListener("fullscreenchange",handleFullScreenChange);
-       
-       return () =>{
-           document.removeEventListener("fullscreenchange",handleFullScreenChange);
-       }
-  },[])
-
+    return () => {
+      document.removeEventListener("fullscreenchange", handleFullScreenChange);
+    };
+  }, []);
 
   useEffect(
     function () {
-   
+      if (hlsRef.current) {
+        hlsRef.current.destroy();
+        hlsRef.current = null;
+      }
+
+      if (videoRef.current && Hls.isSupported()) {
+        setVideoLoading(true);
+        const hls = new Hls();
+        hlsRef.current = hls;
+
+        hls.loadSource(src);
+
+        hls.attachMedia(videoRef.current);
+
+        hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+          console.log("video and hls.js are now bound together !");
+        });
+        hls.on(Hls.Events.MANIFEST_PARSED, function (_event, data) {
+          const qualities = data.levels.map((level, index) => ({
+            height: level.height,
+            level: index,
+          }));
+          setQualityLevels(qualities);
+          setCurrentQuality(hls.autoLevelEnabled ? -1 : hls.currentLevel);
+        });
+        hls.on(Hls.Events.LEVEL_SWITCHED, function (_event, data) {
+          setVideoLoading(false);
+          setCurrentQuality(data.level);
+        });
+        hls.on(Hls.Events.ERROR, function (_event, data) {
+          if (data.fatal) {
+            switch (data.type) {
+              case Hls.ErrorTypes.NETWORK_ERROR:
+                hls.startLoad();
+                setError("A network error cccurred ,Trying to recover");
+                break;
+
+              case Hls.ErrorTypes.MEDIA_ERROR:
+                hls.recoverMediaError();
+                setError("A media error occurred, Trying to recover");
+                break;
+              default:
+                hls.destroy();
+                setError(
+                  " An error occurred cannot play the video please try again",
+                );
+                break;
+            }
+          }
+        });
+      } else {
+        setError("Your browser does not support HLS play back");
+      }
+
+      return () => {
         if (hlsRef.current) {
           hlsRef.current.destroy();
           hlsRef.current = null;
         }
-
-      if (videoRef.current && Hls.isSupported()) {
-
-          setVideoLoading(true);
-          const hls = new Hls();
-          hlsRef.current = hls;
-
-         
-          hls.loadSource(src);
-
-          hls.attachMedia(videoRef.current);
-
-          hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-            console.log("video and hls.js are now bound together !");
-          });
-          hls.on(Hls.Events.MANIFEST_PARSED, function (_event, data) {
-            const qualities = data.levels.map((level, index) => ({
-              height: level.height,
-              level: index,
-            }));
-            setQualityLevels(qualities);
-            setCurrentQuality(hls.autoLevelEnabled ? -1 : hls.currentLevel);
-          });
-          hls.on(Hls.Events.LEVEL_SWITCHED, function (_event, data) {
-            
-            setVideoLoading(false);
-            setCurrentQuality(data.level);
-          });
-          hls.on(Hls.Events.ERROR, function (_event, data) {
-            console.log(data);
-            if (data.fatal) {
-              switch (data.type) {
-                case Hls.ErrorTypes.NETWORK_ERROR:
-                  hls.startLoad();
-                  setError("A network error cccurred ,Trying to recover");
-                  break;
-
-                case Hls.ErrorTypes.MEDIA_ERROR:
-                  hls.recoverMediaError();
-                  setError("A media error occurred, Trying to recover");
-                  break;
-                default:
-                  hls.destroy();
-                  setError(
-                    " An error occurred cannot play the video please try again",
-                  );
-                  break;
-              }
-            }
-          });
-        } else {
-          setError("Your browser does not support HLS play back");
-        
-      }
-
-      return () => {
-         if(hlsRef.current){
-           hlsRef.current.destroy();
-           hlsRef.current = null;
-         }
       };
     },
     [src],
@@ -293,11 +273,8 @@ const formatDuration = useCallback(function(time:number,isCalledToUpdateCurrentD
     videoLoading,
     formatDuration,
     currentDuration,
-    totalDuration
-    
-    
+    totalDuration,
   };
 };
 
 export default useControlVideo;
-
