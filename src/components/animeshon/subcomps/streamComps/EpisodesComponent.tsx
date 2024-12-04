d65aa40e52807a4901c8e7240f52a3f7b1000e54
Sanjay0710Dec <sanjaymike0714@gmail.com>
2024-12-04 11:09:01 +0530

    changed the appearance

--- src/components/animeshon/subcomps/streamComps/EpisodesComponent.tsx
+++ src/components/animeshon/subcomps/streamComps/EpisodesComponent.tsx
@@ -5,30 +5,35 @@ const EpisodesComponent: React.FC<{
   episodes: EpisodeInfo[];
   setEpisodeId: React.Dispatch<React.SetStateAction<string>>;
 }> = React.memo(({ episodes, setEpisodeId }) => {
-    const[episodeTitle,setEpisodeTitle] = useState(episodes[0]?.title || "");
-    const[activeEpisode,setActiveEpisode] = useState(0);
+  const [episodeTitle, setEpisodeTitle] = useState(episodes[0]?.title || "");
+  const [activeEpisode, setActiveEpisode] = useState(0);
   return (
     <section className="mt-4">
-      <div id="episode-title" className="px-2  mb-2 text-2xl lg:text-3xl text-center text-wrap"><span>you're watching:</span> <span  className="text-[#e50000]">{episodeTitle}</span></div>
-    <div className=" lg:max-w-[900px] lg:mx-auto max-h-[350px] md:max-h-[450px]   flex flex-wrap overflow-y-auto  overscroll-y-contain scrollbar-hide border border-orange-400 rounded-md justify-evenly gap-x-2.5 md:gap-x-4  px-2 py-2 gap-y-2 ">
-      {episodes.map((episode, index) => (
-        <div
-          className={`cursor-pointer flex items-center justify-center p-2  h-[36px] w-[36px] md:h-[2.5rem] md:w-[2.5rem] ${index === activeEpisode ? "bg-[#e50000] text-black": null} border border-white   rounded-md`}
-          title={episode.title}
-          key={episode.title + index}
-          onClick={function () {
-            setEpisodeTitle(episode.title);
-            setActiveEpisode(index);
-            setEpisodeId(episode.episodeId);
-          }}
-        >
-          {episode.number}
-        </div>
-      ))}
-    </div>
+      <div
+        id="episode-title"
+        className="px-2 text-2xl lg:text-3xl text-center text-wrap max-w-[900px] w-full mx-auto mb-2"
+      >
+        <span>you're watching:</span>
+        <span className="text-pink-300 ml-2">{episodeTitle}</span>
+      </div>
+      <div className="max-w-[900px] mx-auto max-h-[350px] md:max-h-[450px] grid grid-cols-2 md:grid-cols-3 overflow-y-auto  overscroll-y-contain scrollbar-hide border border-gray-300 rounded-md  gap-x-2.5 md:gap-x-4  px-2 py-2 gap-y-2 ">
+        {episodes.map((episode, index) => (
+          <div
+            className={`text-lg md:text-xl font-semibold cursor-pointer flex flex-col items-center justify-center gap-y-2 p-1  ${index === activeEpisode ? "bg-pink-300 text-black" : ""} border border-white rounded-md`}
+            key={episode.title + index}
+            onClick={function () {
+              setEpisodeTitle(episode.title);
+              setActiveEpisode(index);
+              setEpisodeId(episode.episodeId);
+            }}
+          >
+            <span>{episode.number}</span>
+            <span>{episode.title}</span>
+          </div>
+        ))}
+      </div>
     </section>
   );
 });
 
 export default EpisodesComponent;
-
--- src/components/animeshon/subcomps/streamComps/VideoComponent.tsx
+++ src/components/animeshon/subcomps/streamComps/VideoComponent.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import useControlVideo from "../../../../hooks/useControlVideo";
 import useEpisodeStreamingLink from "../../../../hooks/useEpisodeStreamingLink";
-import { ChevronsLeft, ChevronsRight, Maximize, Minimize, Pause, Play, Settings, Volume, Volume1, Volume2, VolumeX } from "lucide-react";
+import {
+  ChevronsLeft,
+  ChevronsRight,
+  Maximize,
+  Minimize,
+  Pause,
+  Play,
+  Settings,
+  Volume,
+  Volume1,
+  Volume2,
+  VolumeX,
+} from "lucide-react";
 
 const VideoComponent: React.FC<{ src: string }> = React.memo(({ src }) => {
-  
   const {
     loading,
     streamingErrorMessage,
@@ -12,25 +23,19 @@ const VideoComponent: React.FC<{ src: string }> = React.memo(({ src }) => {
     setWatchCategory,
   } = useEpisodeStreamingLink(src);
   if (loading) {
-    return (
-      <section className=" mb-4 h-[300px md:h-[500px] lg:max-w-[900px] text-xl lg:mx-auto  w-full  flex items-center justify-center shadow-sm shadow-red-400">
-        loading...
-      </section>
-    );
+    return <VideoLoadingError content="loading..." />;
   }
   if (streamingErrorMessage) {
-    return (
-      <section className="mb-4 h-[300px md:h-[500px] lg:max-w-[900px] text-xl lg:mx-auto w-full flex items-center justify-center shadow-sm shadow-red-400">
-        {streamingErrorMessage}
-      </section>
-    );
+    return <VideoLoadingError content={streamingErrorMessage} />;
   }
   return (
     <React.Fragment>
-       
-        {videoStreamingLink && <VideoPlayer videoStreamingLink={videoStreamingLink}setWatchCategory={setWatchCategory}/> }
-      
-      
+      {videoStreamingLink && (
+        <VideoPlayer
+          videoStreamingLink={videoStreamingLink}
+          setWatchCategory={setWatchCategory}
+        />
+      )}
     </React.Fragment>
   );
 });
@@ -64,54 +69,75 @@ const VideoPlayer: React.FC<{
     videoLoading,
     formatDuration,
     currentDuration,
-    totalDuration
+    totalDuration,
   } = useControlVideo(videoStreamingLink);
-    
- 
+
   return (
-    <section ref={videoContainerRef}
-      id="video-player" tabIndex={-1}
-      className={`${fullScreen ? "full-screen" : "lg:max-w-[900px] lg:mx-auto "} h-[300] md:h-[500px] relative text-white ${videoLoading ? "cursor-wait":"cursor-pointer"}  w-full  mb-4 shadow-sm shadow-red-400`}
+    <section
+      ref={videoContainerRef}
+      id="video-player"
+      tabIndex={-1}
+      className={`${fullScreen ? "full-screen" : "max-w-[900px] mx-auto "}  relative text-white ${videoLoading ? "cursor-wait" : "cursor-pointer"}  w-full  mb-4 rounded-md border border-[rgba(255,255,255,0.4)]`}
       onKeyDown={handleKeyDown}
-      onClick={togglePlayPause}>
+      onClick={togglePlayPause}
+    >
       <video
         ref={videoRef}
         playsInline
         onTimeUpdate={handleVideoTimeUpdate}
-        onLoadedData={function(){formatDuration(videoRef?.current?.duration || 0)}}
-        className={`border-none  text-white w-full h-full`}
-        
+        onLoadedData={function () {
+          formatDuration(videoRef?.current?.duration || 0);
+        }}
+        className="border-none  text-white w-full h-full rounded-md"
       >
         Your browser does not support video tag
       </video>
-      {error && <div className="w-full my-3 text-xl font-semibold absolute top-1/2 text-center">{error !== "unknown" ? (`${error}, refresh the page`) : "please refresh the page"}</div>}
+      {error && (
+        <div className="w-full my-3 text-xl font-semibold absolute top-1/2 text-center">
+          {error !== "unknown"
+            ? `${error}, refresh the page`
+            : "please refresh the page"}
+        </div>
+      )}
       <div
         id="video-controls-container"
-        className={`${videoPaused ? "opactiy-100" : "opacity-0"} absolute left-0 right-0 bottom-0 w-full px-2 py-1 z-100`}>
-        <div id="progressbar-container" className=" mb-2">
+        className={`${videoPaused ? "opactiy-100" : "opacity-0"} absolute left-0 right-0 bottom-0 w-full px-2 py-1 z-100 ${videoLoading ? "hidden" : "block"}`}
+      >
+        <div id="progressbar-container" className="mb-2">
           <input
             type="range"
-            className="block  w-full"
+            className="block  w-full h-[10px] opacity-80"
             id="progres-bar"
             min={0}
             max={100}
             value={videoProgress}
             onChange={handleInputProgressBarChange}
-            onClick={function (e: React.MouseEvent<HTMLInputElement>) { e.stopPropagation() }}
-            onKeyDown={function(e:React.KeyboardEvent<HTMLInputElement>){e.stopPropagation()}}
+            onClick={function (e: React.MouseEvent<HTMLInputElement>) {
+              e.stopPropagation();
+            }}
+            onKeyDown={function (e: React.KeyboardEvent<HTMLInputElement>) {
+              e.stopPropagation();
+            }}
           />
         </div>
 
-        <div id="controls" className="  flex justify-between items-center">
-
+        <div
+          id="controls"
+          className="  flex justify-between items-center flex-row-reverse bg-[rgba(0,0,0,0.3)] p-1 rounded"
+        >
           <div className="flex items-center justify-between">
-            <button onClick={function (e: React.MouseEvent<HTMLButtonElement>) {
-              e.stopPropagation();
-              togglePlayPause();
-            }} className="h-[30px] w-[30px]">
+            <button
+              onClick={function (e: React.MouseEvent<HTMLButtonElement>) {
+                e.stopPropagation();
+                togglePlayPause();
+              }}
+              className="h-[30px] w-[30px]"
+            >
               {videoPaused ? <Play /> : <Pause />}
             </button>
-            <button title="-5" className="h-[30px] w-[30px]"
+            <button
+              title="-5"
+              className="h-[30px] w-[30px]"
               onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
                 e.stopPropagation();
                 skipVideo5seconds(-5);
@@ -119,7 +145,9 @@ const VideoPlayer: React.FC<{
             >
               <ChevronsLeft />
             </button>
-            <button title="+5" className="h-[30px] w-[30px]"
+            <button
+              title="+5"
+              className="h-[30px] w-[30px]"
               onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
                 e.stopPropagation();
                 skipVideo5seconds(5);
@@ -127,12 +155,26 @@ const VideoPlayer: React.FC<{
             >
               <ChevronsRight />
             </button>
-              
+
             <div id="volume-container" className="flex gap-x-0.5 items-center">
-               <button id="volume-btn" className="h-[30px] w-[30px]" onClick={function(e:React.MouseEvent<HTMLButtonElement>){
+              <button
+                id="volume-btn"
+                className="h-[30px] w-[30px]"
+                onClick={function (e: React.MouseEvent<HTMLButtonElement>) {
                   e.stopPropagation();
-                    handleVolumeMute();
-               }}> {currentVolume  > 80 ? <Volume2/> : currentVolume > 45 ? <Volume1/> : currentVolume > 0 ? <Volume/>:<VolumeX/> }</button>
+                  handleVolumeMute();
+                }}
+              >
+                {currentVolume > 80 ? (
+                  <Volume2 />
+                ) : currentVolume > 45 ? (
+                  <Volume1 />
+                ) : currentVolume > 0 ? (
+                  <Volume />
+                ) : (
+                  <VolumeX />
+                )}
+              </button>
               <input
                 type="range"
                 id="volume-bar"
@@ -140,69 +182,102 @@ const VideoPlayer: React.FC<{
                 max={100}
                 value={currentVolume}
                 onChange={handleVolumeChange}
-                onClick={function (e: React.MouseEvent<HTMLInputElement>) { e.stopPropagation() }}
-                onKeyDown={function(e:React.KeyboardEvent<HTMLInputElement>){e.stopPropagation()}}
+                onClick={function (e: React.MouseEvent<HTMLInputElement>) {
+                  e.stopPropagation();
+                }}
+                onKeyDown={function (e: React.KeyboardEvent<HTMLInputElement>) {
+                  e.stopPropagation();
+                }}
               />
             </div>
             <div id="resolution-sub-dub-speed" className="relative ml-1">
-              <button className="h-[30px] w-[30px] flex items-center" onClick={function (e: React.MouseEvent<HTMLButtonElement>) {
-                e.stopPropagation();
-                setShowSettings((showSetting) => !showSetting);
-              }}> <Settings /> </button>
-              {showSetting && <div className="absolute h-[185px] w-[120px] -top-[210px] -right-[20px] rounded flex flex-col justify-evenly text-white border border-white bg-black">
-                <select className=" text-white bg-black border-b border-white"
-                  value={currentQuality}
-                  onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-                    changeQuality(Number(e.target.value));
-                  }} onClick={function (e: React.MouseEvent<HTMLSelectElement>) { e.stopPropagation() }}>
-                  <option key="-1" value="-1">
-                    Auto
-                  </option>
-                  {qualityLevels.map((quality) => (
-                    <option key={quality.level} value={quality.level}>
-                      {quality.height}
+              <button
+                className="h-[30px] w-[30px] flex items-center"
+                onClick={function (e: React.MouseEvent<HTMLButtonElement>) {
+                  e.stopPropagation();
+                  setShowSettings((showSetting) => !showSetting);
+                }}
+              >
+                <Settings />
+              </button>
+              {showSetting && (
+                <div className="absolute h-[185px] w-[120px] -top-[210px] -right-[20px] rounded flex flex-col justify-evenly text-white border border-white bg-black">
+                  <select
+                    className=" text-white bg-black border-b border-white outline-none"
+                    value={currentQuality}
+                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+                      changeQuality(Number(e.target.value));
+                    }}
+                    onClick={function (e: React.MouseEvent<HTMLSelectElement>) {
+                      e.stopPropagation();
+                    }}
+                  >
+                    <option key="-1" value="-1">
+                      Auto
                     </option>
-                  ))}
-                </select>
+                    {qualityLevels.map((quality) => (
+                      <option key={quality.level} value={quality.level}>
+                        {quality.height}
+                      </option>
+                    ))}
+                  </select>
 
-
-                <button
-                  className="px-3 py-2 border-b border-white"
-                  onClick={function () {
-                    setWatchCategory("sub");
-                  }}
-                >
-                  SUB
-                </button>
-                <button
-                  className="px-3 py-2"
-                  onClick={function () {
-                    setWatchCategory("dub");
-                  }}
-                >
-                  DUB
-                </button>
-              </div>}
+                  <button
+                    className="px-3 py-2 border-b border-white"
+                    onClick={function () {
+                      setWatchCategory("sub");
+                    }}
+                  >
+                    SUB
+                  </button>
+                  <button
+                    className="px-3 py-2"
+                    onClick={function () {
+                      setWatchCategory("dub");
+                    }}
+                  >
+                    DUB
+                  </button>
+                </div>
+              )}
             </div>
-            <button id="minimize-maximize" className="h-[30px] w-[30px]" onClick={function (e: React.MouseEvent<HTMLButtonElement>) {
-              e.stopPropagation();
-              toggleFullScreen()
-            }}>
+            <button
+              id="minimize-maximize"
+              className="h-[30px] w-[30px]"
+              onClick={function (e: React.MouseEvent<HTMLButtonElement>) {
+                e.stopPropagation();
+                toggleFullScreen();
+              }}
+            >
               {fullScreen ? <Minimize /> : <Maximize />}
             </button>
-           
           </div>
-          <div className="flex items-center">
-          {totalDuration &&  <div id="duration-container" className="flex gap-x-1 items-center" onClick={function(e:React.MouseEvent<HTMLDivElement>){
-              e.stopPropagation();
-           }}>
-               <div id="current-time">{currentDuration}</div>
+          <div className="flex items-center max-w-[100px] w-full">
+            {totalDuration && (
+              <div
+                id="duration-container"
+                className="flex gap-x-[0.3rem] items-center w-full"
+                onClick={function (e: React.MouseEvent<HTMLDivElement>) {
+                  e.stopPropagation();
+                }}
+              >
+                <span id="current-time" className="max-w-[40px] w-full">
+                  {currentDuration}
+                </span>
                 <span>/</span>
-               <div id="total-time">{totalDuration}</div>
-            </div>}
+                <span id="total-time">{totalDuration}</span>
+              </div>
+            )}
           </div>
         </div>
       </div>
     </section>
   );
 });
+const VideoLoadingError: React.FC<{ content: string }> = ({ content }) => {
+  return (
+    <section className=" h-[300px] md:h-[500px] mb-4 max-w-[900px] text-xl mx-auto  w-full  flex items-center justify-center rounded-md border border-[rgba(255,255,255,0.4)]">
+      {content}
+    </section>
+  );
+};
